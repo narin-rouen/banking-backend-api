098181aeff5b4d4dd189ccb8419e4742de5a407c
narin-rouen <figfdhb@gmail.com>
2026-02-10 16:51:30 +0100

    Relocate Project Structure

---
+++ src/main/java/com/narinrouen/bankingapi/BankingBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.narinrouen.bankingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narinrouen/bankingapi/BankingBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narinrouen.bankingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
